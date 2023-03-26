14be29bf1696809e7da6a8ce2e2b20250f5bbd54
MuchinaWamburia <lucymuchina20@gmail.com>
2023-03-27 01:26:16 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    # Camper routes
+    get '/campers', to: 'camper#index'
+    get '/campers/:id', to: 'camper#show'
+    post '/campers', to: 'camper#create'
+  
+    # Activity routes
+    get '/activities', to: 'activity#index'
+    delete '/activities/:id', to: 'activity#destroy'
+  
+    # Signup route
+    post '/signups', to: 'signup#create'
+  
 end
